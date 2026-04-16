bce0c31cf12c1ba658786ad0d4cf025f7a4a3fc6
kanekiken70007 <kanekiken70007@gmail.com>
2026-04-16 20:22:34 +0300

    a simple terminal

---
+++ terminal.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdlib.h>   // Add this for exit()
+
+int main() {
+
+    while (1){
+        printf("Shell$");
+        char command[80];
+        fgets(command,sizeof(command), stdin);
+
+        int pid =fork();
+
+         int pos =strcspn(command,"\n");  
+        command[pos] = 0;
+
+        if (pid==0){
+            
+            execlp(command,command, NULL);
+            printf("command not found\n");
+            exit(0);
+        } else {
+         wait(NULL);
+        }
+        
+
+
+
+    }
+
+
+    return 0;
+}
